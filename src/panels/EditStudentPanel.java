57d34e44c247e004734b551705f8774f5583858f
DEIMZ CALIBJO MILOS <2010708@cc.usls.edu>
2026-03-25 15:09:40 +0800

    Edit EditStudent

--- src/panels/EditStudentPanel.java
+++ src/panels/EditStudentPanel.java
@@ -36,7 +36,7 @@ public class EditStudentPanel extends JPanel {
     add(title, BorderLayout.NORTH);
 
     // Table
-    String[] columns = { "Student ID", "Name", "Age", "Contact Number", "School Name" };
+    String[] columns = { "Student ID", "Name", "Age",  "Contact Number", "School Name" };
     tableModel = new DefaultTableModel(columns, 0) {
       @Override
       public boolean isCellEditable(int row, int column) {
